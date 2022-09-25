303bb3e0b93330aace3b79d553e0ec0a50901b4d
= <=>
2022-09-25 09:22:14 +0000

    初級程度の網羅。

--- main.py
+++ main.py
@@ -1,26 +1,58 @@
-from fastapi import FastAPI
+from typing import Union
+from fastapi import FastAPI, Header, Cookie, Response
 from pydantic import BaseModel
+from fastapi.responses import *
 
 app = FastAPI()
 
 
-class Item(BaseModel):
+class pokemonStruct(BaseModel):
+    number: int
     name: str
-    price: float
-    is_offer: bool = None
+    type1: str
+    type2: str
 
 
-@app.get("/")
+@app.get("/pokemon/{name:path}")
+def read_root(name: str):
+    return "{}!!!".format(name)
+
+@app.post("/")
+def read_root(poke: pokemonStruct):
+    return "図鑑番号「{}」は「{}」で、タイプ1は「{}」、タイプ2は「{}」ですね、、、".format(poke.number, poke.name, poke.type1, poke.type2)
+
+@app.get("/cookie")
+def read_rott(response: Response, user_identifier: Union[str, None] = Cookie(default=None)):
+    if user_identifier != None:
+        return "your cookie -> {}".format(user_identifier)
+    else:
+        response.set_cookie(key="user_identifier", value="cookie_value_hogehoge")
+        return "cookie set..."
+
+
+@app.get("/html", response_class=HTMLResponse)
 def read_root():
-    return {"Hello": "World"}
+    return HTMLResponse(content="<html><title>FastAPI</title><body><h1>Hello World</h1></body></html>", status_code=200)
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: str = None):
-    return {"item_id": item_id, "q": q}
+@app.get("/plain", response_class=PlainTextResponse)
+def read_root():
+    return "PLAIN TEXT"
 
 
-@app.put("/items/{item_id}")
-def update_item(item_id: int, item: Item):
-    return {"item_name": item.name, "item_id": item_id}
+@app.get("/redirect", response_class=RedirectResponse)
+def read_root():
+    return RedirectResponse("/pokemon/chikorita")
+
+@app.get("/stream")
+def read_root():
+    def iterfile():
+        with open("data/moku.jpeg", mode="rb") as binary:
+            yield from binary
+    return StreamingResponse(iterfile(), media_type="image/jpeg")
+
+
+@app.get("/file")
+async def read_root():
+    return FileResponse("data/pika.jpeg")
 
