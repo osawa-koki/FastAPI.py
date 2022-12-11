d1998bc87b0ff3c16d02ee83470babd639007c52
osawa-koki <github@osawa.dev>
2022-12-11 20:21:36 +0900

    小さな修正。

--- httpClient.py
+++ httpClient.py
@@ -5,10 +5,10 @@ domain = "localhost:8000"
 path = ""
 
 payload = {
-	"number": 25,
-	"name": "ピカチュウ",
-	"type1": "でんき",
-	"type2": "なし"
+    "number": 25,
+    "name": "ピカチュウ",
+    "type1": "でんき",
+    "type2": "なし"
 }
 
 response = requests.post("http://{}/{}".format(domain, path), data=json.dumps(payload))
